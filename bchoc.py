ebcd7f3b3d649a07c6fba0d768d90fee198d813a
bsmalkin <77949512+bsmalkin@users.noreply.github.com>
2022-10-15 14:22:32 -0700

    Update bchoc.py

--- bchoc.py
+++ bchoc.py
@@ -1,16 +1,52 @@
-class Node:    
-	def __init__(self,value):        
-		self.value = value        
-		self.next = None
-		
-class LinkedList:
-	def __init__(self,head=None):
-		self.head = head    
-		def append(self, new_node):
-            current = self.head
-            if current:
-                while current.next:
-                    current = current.next
-                current.next = new_node
-            else:
-                self.head = new_node
+import calendar
+import time
+
+class Block:
+    def __init__(self, prev_hash, time_stamp, case_id, item_id, state, data_length, data):
+        self.prev_hash = prev_hash
+        self.time_stamp = time_stamp
+        self.case_id = case_id
+        self.item_id = item_id
+        self.state = state
+        self.data_length = data_length
+        self.data = data
+        self.next = None
+
+class Blockchain:
+    def __init__(self, head=None):
+        self.head = head
+    def add(self, new_block): # adds a new block to the blockchain
+        current = self.head
+        if current:
+            while current.next:
+                current = current.next
+            current.next = new_block
+        else:
+            self.head = new_block
+    def checkout(self, passed_item_id): # marks a block state as "CHECKED OUT"
+        current = self.head
+        while (current.next) and (current.item_id != passed_item_id):
+            current = current.next
+        if current.item_id == passed_item_id:
+            current.state = "CHECKEDOUT"
+    def checkin(self, item_id): # marks a block state as "CHECKED IN"
+        current = self.head
+        while (current.next) and (current.item_id != passed_item_id):
+            current = current.next
+        if current.item_id == passed_item_id:
+            current.state = "CHECKEDIN"
+
+    def log():
+
+    def remove(self, item_id): # removes a block
+        current = self.head
+        while (current.next) and (current.item_id != passed_item_id):
+            prev = current
+            current = current.next
+        if current.item_id == passed_item_id:
+            if current.state == "CHECKEDIN":
+                prev = current.next
+
+    def init():
+
+    def verify():
