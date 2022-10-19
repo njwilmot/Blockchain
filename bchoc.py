33ad0034acafbe612535c63334b4a4e9646ca476
bsmalkin <77949512+bsmalkin@users.noreply.github.com>
2022-10-18 22:01:18 -0700

    Update bchoc.py

--- bchoc.py
+++ bchoc.py
@@ -1,5 +1,7 @@
-import calendar
-import time
+from datetime import timezone
+import datetime
+import sys
+
 
 class Block:
     def __init__(self, prev_hash, time_stamp, case_id, item_id, state, data_length, data):
@@ -12,11 +14,17 @@ class Block:
         self.data = data
         self.next = None
 
+
 class Blockchain:
+    block_chain_size = 0
+
     def __init__(self, head=None):
         self.head = head
-        
-    def add(self, new_block): # adds a new block to the blockchain
+
+    def is_empty(self):
+        return self.__sizeof__()
+
+    def add(self, new_block):  # adds a new block to the blockchain
         current = self.head
         if current:
             while current.next:
@@ -24,32 +32,71 @@ class Blockchain:
             current.next = new_block
         else:
             self.head = new_block
-            
-    def checkout(self, passed_item_id): # marks a block state as "CHECKED OUT"
+
+    block_chain_size += 1
+
+    def checkout(self, passed_item_id):  # marks a block state as "CHECKED OUT"
         current = self.head
-        while (current.next) and (current.item_id != passed_item_id):
+        while current.next and (current.item_id != passed_item_id):
             current = current.next
         if current.item_id == passed_item_id:
             current.state = "CHECKEDOUT"
-            
-    def checkin(self, item_id): # marks a block state as "CHECKED IN"
+
+    def checkin(self, passed_item_id):  # marks a block state as "CHECKED IN"
         current = self.head
-        while (current.next) and (current.item_id != passed_item_id):
+        while current.next and (current.item_id != passed_item_id):
             current = current.next
         if current.item_id == passed_item_id:
             current.state = "CHECKEDIN"
 
-    def log():
+    # def log():
 
-    def remove(self, item_id): # removes a block
+    def remove(self, passed_item_id):  # removes a block
         current = self.head
-        while (current.next) and (current.item_id != passed_item_id):
+        while current.next and (current.item_id != passed_item_id):
             prev = current
             current = current.next
         if current.item_id == passed_item_id:
             if current.state == "CHECKEDIN":
                 prev = current.next
 
-    def init():
+    # def init(self):
 
-    def verify():
+    # def verify(self):
+
+
+def main():
+    user_input = sys.argv
+    user_action = user_input[2]
+
+    blockchain = Blockchain()
+
+    match user_action:
+        case 'add':
+            # print("add")
+            case_id = user_input[4]
+            item_id = user_input[6]
+            time = maya.MayaDT.from_datetime(datetime.utcnow())
+            if blockchain.block_chain_size > 0:
+                block = Block(None, time, case_id, item_id, "CHECKEDIN", )
+            else:
+                block = Block(None, time, None, None, "INITIAL", 14, "Initial block")
+
+            blockchain.add(block)
+
+        case 'checkout':
+            print("add")
+        case 'checkin':
+            print("add")
+        case 'log':
+            print("log")
+        case 'remove':
+            print("remove")
+        case 'init':
+            print("init")
+        case 'verify':
+            print("verify")
+
+
+if __name__ == "__main__":
+    main()
